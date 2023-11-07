c5ffc2a6de8b6b275a53f65b39f11cc81b6d1f2f
Albina Vivdenko <albinka3322@gmail.com>
2023-11-07 21:56:15 +0100

    л

--- js/task-09.js
+++ js/task-09.js
@@ -2,35 +2,21 @@ const buttonEl = document.querySelector('.change-color')
 
 const spanEl = document.querySelector('.color')
 
-console.log(buttonEl);
-console.log(spanEl);
 
 const body = document.querySelector('body')
 
-console.log("body");
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
 
 buttonEl.addEventListener("click", handleDeClick)
 
 function handleDeClick() {
 
-    function getRandomHexColor() {
-        return `#${Math.floor(Math.random() * 16777215)
-            .toString(16)
-            .padStart(6, 0)}`;
-    }
-
     body.style.backgroundColor = getRandomHexColor();
 
 
     spanEl.textContent = body.style.backgroundColor;
-
-    console.log("spanEl.textContent");
-
-
-
-
-
-
 }
-
